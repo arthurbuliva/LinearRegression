75c06817187f232d4d45fbd8b446565a2f56baf3
Arthur Buliva <arthurbuliva@gmail.com>
2017-07-22 19:33:19 +0200

    Initial commit

--- src/linearregression/weka/Predictor.java
+++ src/linearregression/weka/Predictor.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This is a test for Machine learning linear regression prediction using
+ * the WEKA library
+ * 
+ * @author Arthur Buliva
  */
 package linearregression.weka;
 
-/**
- *
- * @author Arthur Buliva
- */
 import java.io.BufferedReader;
 import java.io.FileReader;
 import weka.core.Instance;
